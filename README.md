b195b87d28951d8cf624ad440da79dfd957b4ccb
H8H <mc2m382@chomeyer.de>
2014-08-28 13:42:46 +0200

    Wrote some code for the hardware control panel

--- README.md
+++ README.md
@@ -3,12 +3,12 @@ CarMPD
 
 **Not Ready To Use, its still under heavy development**
 
-Build up a headless mpd server with a webclient (accessible via wireless lan) and a hardware control panel out-of-the-box onto your raspberry pi.
+Build up a headless out-of-the-box mpd server with a webclient (accessible via wireless lan) and a hardware control panel onto your raspberry pi.
 
 Features
 --------
 * Out-of-the-box, just fire up one line and you have all the features
-* AccessPoint, if you have a AP-supported wireless dongle, you can control mpd over the air
+* AccessPoint, if you have an AP-supported wireless dongle, you can control mpd over the air
 * Control mpd with some pushbuttons (GPIO)
 * Easily upload your songs (not implemented yet)
 
--- client/console-unit.py
+++ client/console-unit.py
@@ -1,25 +1,115 @@
 #!/usr/bin/env python
 import time
-import mpd
+import os
+import mpd 
+import RPi.GPIO as GPIO
 
+def next_button(id):
+	"""
+		Handles the two-state pullup button
+		Short push - Next song
+		Long  push - Seek current song 
+	"""
+	# TODO - Silly mutex, i will improve it
+	global btn_next
+	if btn_next:
+		return
+	btn_next = True
+
+	# Switches the light on
+	GPIO.output(24,GPIO.HIGH)
+
+	time.sleep(0.2)
+	
+	# Is button still pushed - start seeking
+	if not GPIO.input(id):
+		while not GPIO.input(id):
+			client.seekcur("+10")
+			time.sleep(1)
+	else:
+		client.next()
+
+	# Turns the led off
+	GPIO.output(24,GPIO.LOW)
+
+	# Release silly mutex
+	btn_next = False
+
+def prev_button(id):
+	"""
+		Handles the two-state pullup prev_button
+		Short push - Next song
+		Long  push - Poweroff Pi
+	"""
+	# TODO - Silly mutex, i will improve it
+	global btn_prev
+	if btn_prev:
+		return
+	btn_prev = True
+
+	# Switches the led on
+	GPIO.output(26,GPIO.HIGH)
+
+	# Counter how long the button should be pushed 
+	# to poweroff Pi
+	times=10
+
+	time.sleep(0.2)
+	
+	# Is button still pushed - start countdown
+	if not GPIO.input(id):
+		while not GPIO.input(id):
+			times -= 1
+			time.sleep(0.2)
+		if times <= 0:
+			GPIO.output(24,GPIO.HIGH)
+			GPIO.output(26,GPIO.HIGH)
+			os.system("/usr/bin/poweroff")
+			return
+
+	# Previous song
+	client.previous()
+
+	# Turns the led off
+	GPIO.output(26,GPIO.LOW)
+
+	# Release silly mutex
+	btn_prev = False
+
+# Set up the Pi
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(16, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(18, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(24, GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
+
+#Register Events
+GPIO.add_event_detect(16, GPIO.FALLING, callback = next_button, bouncetime = 200)
+GPIO.add_event_detect(18, GPIO.FALLING, callback = prev_button, bouncetime = 200)
+
+# Turns the led off
+GPIO.output(26,GPIO.LOW)
+GPIO.output(24,GPIO.LOW)
+
+# Inits the silly mutex
+btn_next = False
+btn_prev = False 
+
+# Setup mpd client
 client = mpd.MPDClient()
 client.connect("localhost", 6600)
-#client.timeout = 5 
-print(client.mpd_version)
+
 try:
-    client.ping()
-except mpd.ConnectionError:
-    client.connect("localhost", 6600)
+	while True:
+		try:
+			client.ping()
+		except mpd.ConnectionError:
+			client.connect("localhost", 6600)
+			time.sleep(55)
 
-print('Go go')
-client.seekcur("+10")
-client.seekcur("+10")
-client.seekcur("+10")
-client.seekcur("+10")
-client.seekcur("+10")
-client.seekcur("+10")
-client.seekcur("+10")
-client.seekcur("+10")
+except KeyboardInterrupt:
+	pass
 
+GPIO.cleanup()
 client.close()
-client.disconnect() 
+client.disconnect()
